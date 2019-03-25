0008083c2719e29f61f044ae2d509cfe2ae72e55
Valera <v-chupurnov@edadeal.ru>
2019-03-25 20:08:29 +0300

    New version 1.0.4. Read more https://github.com/stylus/stlint/releases/tag/1.0.4

--- rules/testRule.js
+++ rules/testRule.js
@@ -1,9 +1,9 @@
-import { Rule } from "stlint";
-
-class TestRule extends Rule {
-	checkLine(line) {
+function TestRule() {
+	this.checkLine = (line) => {
 		if (line.lineno === 1) {
 			this.msg('Test error on test line', 1, 1, line.length);
 		}
 	};
 }
+
+module.exports.TestRule = TestRule;
