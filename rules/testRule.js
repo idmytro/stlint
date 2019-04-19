6a6ce11cd03d2d1e238302181d8ac7284c149d82
Valera <v-chupurnov@edadeal.ru>
2019-04-19 15:25:01 +0300

    New version 1.0.8. Read more https://github.com/stylus/stlint/releases/tag/1.0.8

--- rules/testRule.js
+++ rules/testRule.js
@@ -1,7 +1,25 @@
+const Rgb = require('stlint').ast.RGB;
+
 function TestRule() {
+	nodesFilter = ['rgb']; // can be one of https://github.com/stylus/stlint/tree/master/src/core/ast
+
+	/**
+	 * Check the AST node
+	 * @param node
+	 */
+	this.checkNode = (node) => {
+		if (node instanceof Rgb) {
+			// this.msg('Test error on test node', node.lineno, node.column, node.line.length);
+		}
+	};
+
+	/**
+	 * Check every line
+	 * @param line
+	 */
 	this.checkLine = (line) => {
 		if (line.lineno === 1) {
-			// this.msg('Test error on test line', 1, 1, line.length);
+			// this.msg('Test error on test line', line.lineno, 1, line.line.length);
 		}
 	};
 }
