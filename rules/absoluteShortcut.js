e0b95997f2f144767aabc7ebf48d8c9431e6e9a5
Valera <v-chupurnov@edadeal.ru>
2019-04-29 19:04:18 +0300

    Added absoluteShortcut rule

---
+++ rules/absoluteShortcut.js
@@ -0,0 +1,76 @@
+const  { Property, Value } = require('stlint').ast;
+
+const AbsoluteKeys = [
+	'top',
+	'right',
+	'bottom',
+	'left'
+];
+
+function AbsoluteShortcut() {
+	this.nodesFilter = ['block'];
+
+	/**
+	 * Check the AST node
+	 * @param node
+	 */
+	this.checkNode = (node) => {
+		const orderKeys = this.conf.orderKeys || AbsoluteKeys;
+
+		let
+			hasAbsolute = false,
+			sizes = [];
+
+		node.nodes.forEach((child) => {
+			if (child instanceof Property || child instanceof Value) {
+				if (orderKeys.includes(child.key)) {
+					sizes.push(child);
+				} else if (child.key === 'position' && child.value.toString() === 'absolute') {
+					hasAbsolute = child;
+					sizes.push(child);
+				}
+			}
+		});
+
+		if (hasAbsolute) {
+			sizes.sort((a, b) => a.lineno - b.lineno);
+
+			let canFix = true;
+
+			for (let i = 1; i < sizes.length; i+= 1) {
+				if (sizes[i].lineno - sizes[i - 1].lineno !== 1) {
+					canFix = false;
+					break;
+				}
+			}
+
+			let fix = null;
+
+			if (canFix) {
+				const
+					ordered = [...sizes];
+
+				ordered.sort((a, b) => orderKeys.indexOf(a.key) - orderKeys.indexOf(b.key));
+
+				fix = ordered.reduce((res, prop) => {
+					if (prop.key !== 'position') {
+						res.push(prop.key + ' ' + prop.value);
+					}
+
+					return res;
+				}, ['absolute']).join(' ');
+			}
+
+			this.msg(
+				'Replace position:absolute to absolute',
+				sizes[0].lineno,
+				sizes[0].column,
+				sizes[sizes.length - 1].line.line.length,
+				fix,
+				sizes[sizes.length - 1].lineno
+			);
+		}
+	};
+}
+
+module.exports.AbsoluteShortcut = AbsoluteShortcut;
---
+++ tests/rules/absoluteShortcutTest.js
@@ -0,0 +1,77 @@
+const { Linter, Content } = require("stlint");
+const { expect } = require("chai");
+const { AbsoluteShortcut } = require("../../rules/absoluteShortcut");
+
+extendsRule(AbsoluteShortcut);
+
+let wrongContent = '.tab\n\tposition absolute\n\ttop 10px\n\tleft 20px\n\tright 10px';
+
+describe('Test absoluteShortcut', () => {
+	it('Should throw error position absolute', () => {
+		const rule = new AbsoluteShortcut({
+			conf: 'always'
+		});
+
+		parseAndRun(
+			wrongContent,
+			rule
+		);
+
+		expect(rule.errors.length).to.be.equal(1);
+	});
+
+	describe('Fix absoluteShortcut', () => {
+		describe('Fix absoluteShortcut', () => {
+			it('Should replace position absolute to absolute', () => {
+				const linter = new Linter({
+					rules: {
+						absoluteShortcut: {
+							conf: "always"
+						}
+					},
+					grep: 'absoluteShortcut',
+					reporter: 'silent',
+					fix: true
+				});
+
+				linter.lint('./tests/test.styl', wrongContent);
+				linter.display(false);
+
+				const response = linter.reporter.response;
+
+				expect(response.passed).to.be.false;
+				expect(response.errors && response.errors.length).to.be.equal(1);
+
+				expect('.tab\n\tabsolute top 10px right 10px left 20px').to.be
+					.equal(linter.fix('./tests/test.styl', new Content(wrongContent)));
+			});
+		});
+		describe('Position ans left,right,top,bottom no in next line', () => {
+			it('Should not replace Position but show error', () => {
+				wrongContent = '.tab\n\tposition absolute\n\tborder 1px solid #ccc\n\ttop 10px\n\tleft 20px\n\tright 10px';
+
+				const linter = new Linter({
+					rules: {
+						absoluteShortcut: {
+							conf: "always"
+						}
+					},
+					grep: 'absoluteShortcut',
+					reporter: 'silent',
+					fix: true
+				});
+
+				linter.lint('./tests/test.styl', wrongContent);
+				linter.display(false);
+
+				const response = linter.reporter.response;
+
+				expect(response.passed).to.be.false;
+				expect(response.errors && response.errors.length).to.be.equal(1);
+
+				expect(wrongContent).to.be
+					.equal(linter.fix('./tests/test.styl', new Content(wrongContent)));
+			});
+		});
+	});
+});
